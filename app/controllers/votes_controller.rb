b69c94a6c6d9f7e4529d96238f07ce691729688f
Joseph Mazrimas & Nick Block <pair+jmazrimas.nicholasblock78@devbootcamp.com>
2016-08-10 16:31:12 -0500

    routes and updates for all votes working

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,21 +1,28 @@
 post '/questions/:id/votes' do
 
-  Question.find(params[:id]).votes.create(user_id: current_user.id, value: value)
+  Question.find(params[:id]).votes.create(user_id: current_user.id, value: params[:vote].to_i)
 
   redirect back
 end
 
-post 'questions/:id/answers/:id/votes' do
+post 'questions/:qid/answers/:aid/votes' do
+
+  # may need to refactor this -- only relying on answerIDs -- not using questions at all
+  Answer.find(params[:aid]).votes.create(user_id: current_user.id, value: params[:vote].to_i)
 
   redirect back
 end
 
-post '/questions/:id/comments/:id/votes' do
+post '/questions/:qid/comments/:cid/votes' do
+
+  Comment.find(params[:cid]).votes.create(user_id: current_user.id, value: params[:vote].to_i)
 
   redirect back
 end
 
-post '/questions/:id/answers/:id/comments/:id/votes' do
+post '/questions/:qid/answers/:aid/comments/:cid/votes' do
+
+  Comment.find(params[:cid]).votes.create(user_id: current_user.id, value: params[:vote].to_i)
 
   redirect back
 end
