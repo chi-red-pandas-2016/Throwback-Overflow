7120b9bc9b958b8e6e78ea46e2d4cffc245f229a
Forrest W & Lauren Daniels <pair+fwortham4.ladaniels@devbootcamp.com>
2016-08-10 10:13:52 -0500

    add polymorphic relationships to classes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ActiveRecord::Base
   # Remember to create a migration!
+  belongs_to :user
+  belongs_to :answer, polymorphic: true
+  belongs_to :question, polymorphic: true
+  has_many :votes, as: :voteable
+
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,8 @@
 class Question < ActiveRecord::Base
   # Remember to create a migration!
+  belongs_to :user
+  has_many :answers
+
+  has_many :comments, as: :commentable
+  has_many :votes, as: :voteable
 end
