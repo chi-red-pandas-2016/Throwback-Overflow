21a31d4ae80bbda1b307fb5b7789751759096cdc
Lauren Daniels <laurenddaniels@gmail.com>
2016-08-11 15:38:13 -0500

    AJAX working for commenting!

--- public/js/application.js
+++ public/js/application.js
@@ -19,4 +19,24 @@ $(document).ready(function() {
       $('#answers-list').prepend(response);
     });// close AJAX request
   }); // END OF ANSWER AJAX
+
+  // AJAX FOR COMMENTS
+  $('form#comment-form').on('submit', function( event ){
+    event.preventDefault();
+    var $form = $(this);
+    var commentData = $form.serialize();
+    $.ajax({
+      method: $form.attr('method'),
+      url: $form.attr('action'),
+      data: commentData
+    }).done( function(response){
+      $form[0].reset();
+      var route = $form.attr('action');
+      if (route.includes("answers")){
+        $('.answer-comments-list').prepend(response);
+      } else {
+        $('.question-comments-list').prepend(response);
+      }
+    }); // close AJAX request
+  }); // END OF COMMENTS AJAX
 });// close on ready
