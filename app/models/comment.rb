5064db760e78fa9be5cf0c607c3ad0a290f2c38e
jmazrimas <joe.mazrimas@gmail.com>
2016-08-11 09:39:29 -0500

    routes for voting on comments cleaner now

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,8 +4,17 @@ class Comment < ActiveRecord::Base
   belongs_to :answer, polymorphic: true
   belongs_to :question, polymorphic: true
   has_many :votes, as: :voteable
-
   validates :text, presence: true
+
+  def parent_question
+    if self.commentable_type = "Answer"
+      Answer.find(commentable_id).question
+    end
+  end
+
+  def vote_total
+    Vote.where(voteable_type: "Comment", voteable_id: self.id).sum(:value)
+  end
 
 
 end
