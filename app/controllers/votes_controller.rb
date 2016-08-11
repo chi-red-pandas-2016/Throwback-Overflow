c5ea63431bf6b30425ee4dfc4d3977aa2c3f53ff
Lauren Daniels <laurenddaniels@gmail.com>
2016-08-11 15:54:41 -0500

    Whoops didn't swicth the branch

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,56 +1,65 @@
 post '/questions/:id/votes' do
+  if logged_in?
+    question = Question.find(params[:id])
 
-  question = Question.find(params[:id])
-
-  if existing = existing_vote(current_user.id, "Question", question.id)
-    existing.update_attribute(:value, params[:vote].to_i)
-    redirect back
+    if existing = existing_vote(current_user.id, "Question", question.id)
+      existing.update_attribute(:value, params[:vote].to_i)
+      redirect back
+    else
+      question.votes.create(user_id: current_user.id, value: params[:vote].to_i)
+      redirect back
+    end
   else
-    question.votes.create(user_id: current_user.id, value: params[:vote].to_i)
-    redirect back
+    @errors << "You must be a member to vote"
   end
-
 end
 
 post '/questions/:qid/answers/:aid/votes' do
+  if logged_in?
+    # may need to refactor this -- only relying on answerIDs -- not using questions at all
+    answer = Answer.find(params[:aid])
 
-  # may need to refactor this -- only relying on answerIDs -- not using questions at all
-  answer = Answer.find(params[:aid])
-
-  if existing = existing_vote(current_user.id, "Answer", answer.id)
-    existing.update_attribute(:value, params[:vote].to_i)
-    redirect back
+    if existing = existing_vote(current_user.id, "Answer", answer.id)
+      existing.update_attribute(:value, params[:vote].to_i)
+      redirect back
+    else
+      answer.votes.create(user_id: current_user.id, value: params[:vote].to_i)
+      redirect back
+    end
   else
-    answer.votes.create(user_id: current_user.id, value: params[:vote].to_i)
-    redirect back
+     @errors << "You must be a member to vote"
   end
-
 end
 
 post '/questions/:qid/comments/:cid/votes' do
 
-  comment = Comment.find(params[:cid])
+  if logged_in?
+    comment = Comment.find(params[:cid])
 
-  if existing = existing_vote(current_user.id, "Comment", comment.id)
-    existing.update_attribute(:value, params[:vote].to_i)
-    redirect back
+    if existing = existing_vote(current_user.id, "Comment", comment.id)
+      existing.update_attribute(:value, params[:vote].to_i)
+      redirect back
+    else
+      comment.votes.create(user_id: current_user.id, value: params[:vote].to_i)
+      redirect back
+    end
   else
-    comment.votes.create(user_id: current_user.id, value: params[:vote].to_i)
-    redirect back
+     @errors << "You must be a member to vote"
   end
-
 end
 
 post '/questions/:qid/answers/:aid/comments/:cid/votes' do
+  if logged_in?
+    Comment.find(params[:cid])
 
-  Comment.find(params[:cid])
-
-  if existing = existing_vote(current_user.id, "Comment", comment.id)
-    existing.update_attribute(:value, params[:vote].to_i)
-    redirect back
+    if existing = existing_vote(current_user.id, "Comment", comment.id)
+      existing.update_attribute(:value, params[:vote].to_i)
+      redirect back
+    else
+      comment.votes.create(user_id: current_user.id, value: params[:vote].to_i)
+      redirect back
+    end
   else
-    comment.votes.create(user_id: current_user.id, value: params[:vote].to_i)
-    redirect back
+     @errors << "You must be a member to vote"
   end
-
 end
